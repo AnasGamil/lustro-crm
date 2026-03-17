bc9e840803c1fc99a4ada49831ecae2e1ab7d942
Anas Gamil <77108915+AnasGamil@users.noreply.github.com>
2026-03-18 00:21:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -920,7 +920,7 @@ def proxy_to_real_crm(endpoint):
 
     try:
         resp = req_lib.request(
-            method='GET' if 'checkinsurance' in endpoint else request.method,
+            method='GET' if 'checkinsurance' in endpoint or 'appointments/available' in endpoint else request.method,
             url=target_url,
             json=body if body else None,
             params=params,
